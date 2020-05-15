5bcd42f63b90dbc7b61c4c0f49c7dbf5d99d5a31
pooja mishra <mishrapooja8128@gmail.com>
2020-05-15 10:45:15 +0530

    first commit

---
+++ login_form/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LoginFormConfig(AppConfig):
+    name = 'login_form'
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\x3\desktop\login form_page\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
